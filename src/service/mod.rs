b69d9fc6d6ee2f7fa8de887460b7fb60c75cbaa5
rndhouse <rndhouse@protonmail.com>
2022-02-01 20:19:35 +0000

    Update dependencies.

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,21 +1,20 @@
-use bson::{doc, ordered::OrderedDocument};
-use mongodb::{error::Error, results::InsertOneResult, Collection};
+use mongodb::{error::Error, results::InsertOneResult, sync::Collection};
 
 #[derive(Clone)]
 pub struct UserService {
-  collection: Collection,
+    collection: mongodb::sync::Collection<bson::Document>,
 }
 
 impl UserService {
-  pub fn new(collection: Collection) -> UserService {
-    UserService { collection }
-  }
+    pub fn new(collection: Collection<bson::Document>) -> UserService {
+        UserService { collection }
+    }
 
-  pub fn create(&self, name: &str) -> Result<InsertOneResult, Error> {
-    self.collection.insert_one(doc! {"name": name}, None)
-  }
+    pub fn create(&self, name: &str) -> Result<InsertOneResult, Error> {
+        self.collection.insert_one(bson::doc! {"name": name}, None)
+    }
 
-  pub fn get(&self) -> Result<Option<OrderedDocument>, Error> {
-    self.collection.find_one(doc! {}, None)
-  }
+    pub fn get(&self) -> Result<Option<bson::Document>, Error> {
+        self.collection.find_one(bson::doc! {}, None)
+    }
 }
